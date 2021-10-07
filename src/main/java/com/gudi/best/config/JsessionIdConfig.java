407ce28d1ee420225411abe0e043392ce44924bc
parkhan <6987410@naver.com>
2021-10-07 10:55:57 +0900

    한솔:: jsessionid 설정 수정

---
+++ src/main/java/com/gudi/best/config/JsessionIdConfig.java
@@ -0,0 +1,28 @@
+package com.gudi.best.config;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
+
+@Configuration
+public class JsessionIdConfig implements WebMvcConfigurer {
+    //j-session 삭제 url에 jessionid 파라미터 붙는 현상 제거
+    @Bean
+    public ServletContextInitializer clearJsession() {
+        return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+                SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
+                sessionCookieConfig.setHttpOnly(true);
+            }
+        };
+    }
+}
