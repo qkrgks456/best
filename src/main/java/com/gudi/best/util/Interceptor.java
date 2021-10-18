252386e0eb72f0c6ec22804f821b33ac2ec5d8b6
parkhan <6987410@naver.com>
2021-10-18 13:01:56 +0900

    한솔:: 인터셉터 처리 완료

--- src/main/java/com/gudi/best/util/Interceptor.java
+++ src/main/java/com/gudi/best/util/Interceptor.java
@@ -5,20 +5,30 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
 
-public class Interceptor implements HandlerInterceptor{
-	// 인터셉터 클래스
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		return true;
-	}
+public class Interceptor implements HandlerInterceptor {
+    // 인터셉터 클래스
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession();
+        String loginId = (String) session.getAttribute("loginId");
+        if (loginId == null) {
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script>alert('로그인 해주세요 ^^'); location.href='/';</script>");
+            out.flush();
+        }
+        return true;
+    }
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		
-	}
-	
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+    }
+
 
 }
