b15b2558a9e15bdc3c5ae2c87a8c85db0131dfa4
DESKTOP-VIG3CFG\USER <ort112@daum.net>
2021-10-21 16:41:01 +0900

    한솔 :: 살려보기

--- src/main/java/com/gudi/best/logic/loveInfo/controller/LoveInfoController.java
+++ src/main/java/com/gudi/best/logic/loveInfo/controller/LoveInfoController.java
@@ -5,12 +5,15 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gudi.best.logic.loveInfo.mapper.LoveInfoMapper;
+import com.gudi.best.util.NewApiUtil;
 import com.gudi.best.util.Scheduler;
 
 @Controller
@@ -19,36 +22,47 @@ public class LoveInfoController {
     @Autowired
     LoveInfoMapper loveInfoMapper;
 
-    @GetMapping("/couple")
-    public String couple() {
-        return "logic/dateInfo/couple";
-    }
+    @GetMapping("/tourPick")
+    public String tourPick(){
+    	HashMap <String,String> header = new HashMap<String,String>();
+    	HashMap <String,String> param = new HashMap<String,String>();
+    	String url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode";
+    	param.put("ServiceKey", "J0GWcsmSKRep08rNleYyFbyLzNo8BcbkWmzsOxqFeJIyHVD3456iyVy+ULCnhVw5y9WsuzCa4ZjugOCCfA0JOA==");
+        param.put("pageNo", "1");
+        param.put("MobileOS", "ETC");
+        param.put("MobileApp", "test");   
+        param.put("areaCode","1");
+        param.put("areaCode","2");
+        /*
+        param.put("areaCode","1");   //서울 
+        param.put("areaCode","2");  //인천
+        param.put("areaCode","3");  // 대전 
+        param.put("areaCode","4"); //대구 
+        param.put("areaCode","5");  // 광주
+        param.put("areaCode","6");  // 부산 
+        param.put("areaCode","7"); // 울산 
+        param.put("areaCode","8"); //세종 특별자치시
+        param.put("areaCode","9"); // 경기도 
+        param.put("areaCode","10");  //강원도 
+        */
+        param.put("_type", "json");
+        
+   
+    	NewApiUtil.sendSeverMsg(url, header, param, "GET");
+    	
+        return "logic/dateInfo/tourPick";
 
-    @GetMapping("/taste")
-    public String taste() {
-        return "logic/dateInfo/taste";
+        
+   
     }
-
-    @GetMapping("/menuPick")
-    public String menuPick() throws Exception {
-        Scheduler scheduler = new Scheduler();
-//		for (int i=1 ; i<=9; i++ ) {
-        String result = scheduler.dataInsert(1);
-        ObjectMapper mapper = new ObjectMapper();
-        HashMap<String, Object> map = mapper.readValue(result, HashMap.class);
-        result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map.get("response"));
-        HashMap<String, Object> map2 = mapper.readValue(result, HashMap.class);
-        result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map2.get("body"));
-        HashMap<String, Object> map3 = mapper.readValue(result, HashMap.class);
-        result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map3.get("items"));
-        HashMap<String, Object> map4 = mapper.readValue(result, HashMap.class);
-        result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map4.get("item"));
-        TypeReference<ArrayList<HashMap<String, Object>>> typeRef = new TypeReference<ArrayList<HashMap<String, Object>>>() {
-        };
-        ArrayList<HashMap<String, Object>> resultMapList = mapper.readValue(result, typeRef);
-        /*loveInfoMapper.dataInsert(resultMapList);*/
-//		}
-        return "logic/dateInfo/menuPick";
+    
+    
+    @GetMapping("/tourMenu")   //관광지
+    public String tourist(String tour,Model model) {
+    	model.addAttribute("tour",tour);
+        return "logic/dateInfo/locationPick";
     }
-
+    
+    
+    
 }
